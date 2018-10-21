e97a1fa18c6aa440e352be69a21fae3604c3f68b
Hank Tadeusiak <hankt@uw.edu>
2018-10-21 12:51:20 -0700

    s

--- js/create-event-btn.js
+++ js/create-event-btn.js
@@ -2,6 +2,14 @@
 (function() {
 
   window.onload = function() {
+    let buttons = document.querySelectorAll(".popup");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].onclick = function() {
+        let name2 = buttons[i].innerText
+        console.log(name2);
+          popup(name2);
+      };
+    }
     document.getElementById("create-event-btn").onclick = function() {
       userInformation()
     };
@@ -12,12 +20,31 @@
     let place = prompt("Please enter your title");
     let clas = prompt("Please enter your class");
     let location = prompt("Please enter your location");
-    let link = document.createElement("a");
     let btn = document.createElement("button");
-    link.href = "hello";
+    let popupHolder = document.createElement("div");
+    popupHolder.id = place;
+    popupHolder.classList.add("hidden");
+    popupHolder.classList.add("simple-pop");
+    let pname = document.createElement("p");
+    pname.innerText = name;
+    popupHolder.appendChild(pname);
+    let ptitle = document.createElement("p");
+    ptitle.innerText = place;
+    popupHolder.appendChild(ptitle);
+    let pclass = document.createElement("p");
+    pclass.innerText = clas;
+    popupHolder.appendChild(pclass);
+    let plocation = document.createElement("p");
+    plocation.innerText = location;
+    popupHolder.appendChild(plocation);
+    document.getElementById("all-popups").appendChild(popupHolder);
     btn.innerText = place;
-    link.appendChild(btn)
-    document.getElementById("event-feed").appendChild(link);
+    document.getElementById("event-feed").appendChild(btn);
+  }
+
+  function popup(name) {
+    let popup = document.getElementById(name);
+    popup.classList.toggle("hidden");
   }
 
 })();
