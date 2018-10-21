91003013f8c5cebbc6431ebab699e9920f8312b7
Sam Wheelock <s9a9m9m9y@gmail.com>
2018-10-20 22:45:06 -0700

    Map Organization

--- js/practice.js
+++ js/practice.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+// The location of Uluru
+var uluru = {lat: 47.6565, lng: -122.31};
+// The map, centered at Uluru
+var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 10, center: uluru});
+// The marker, positioned at Uluru
+var marker = new google.maps.Marker({position: uluru, map: map});
 }
