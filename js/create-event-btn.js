11c4e3c9295d820c6cc3f82bb159bcc62be1743a
Hank Tadeusiak <hankt@uw.edu>
2018-10-21 11:55:19 -0700

    yuop

--- js/create-event-btn.js
+++ js/create-event-btn.js
@@ -3,8 +3,21 @@
 
   window.onload = function() {
     document.getElementById("create-event-btn").onclick = function() {
-      alert("hi");
+      userInformation()
     };
   };
+
+  function userInformation() {
+    let name = prompt("Please enter your name");
+    let place = prompt("Please enter your title");
+    let clas = prompt("Please enter your class");
+    let location = prompt("Please enter your location");
+    let link = document.createElement("a");
+    let btn = document.createElement("button");
+    link.href = "hello";
+    btn.innerText = place;
+    link.appendChild(btn)
+    document.getElementById("event-feed").appendChild(link);
+  }
 
 })();
